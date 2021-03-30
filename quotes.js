5c956b96685707b53e03eeeb9242b368ab3a22b8
emirmoretti <71144640+emirmoretti@users.noreply.github.com>
2021-03-30 18:27:01 -0300

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -4,7 +4,7 @@ let quotes =
     [
         'You learn more from failure than from succes. Dont Let It Stop you',
         'Knowing is not enough; we must apply. Wishing is not enough; we must do. - Johann wolfang von goethe',
-        'tu novia puta con mi pingo se ahoga y vos no decis nada pq sos un toga',
+        "Don't quit",
     ];
 
     btn.addEventListener('click', function(){
